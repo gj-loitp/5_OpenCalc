7e719a2b04b11e296e08c76f5edf3d94384156b0
loitp <www.muathu@gmail.com>
2023-05-01 16:10:58 +0700

    update

--- app/src/main/java/com/roy/OpenCalcApplication.kt
+++ app/src/main/java/com/roy/OpenCalcApplication.kt
@@ -1,9 +1,10 @@
-package com.darkempire78.opencalculator
+package com.roy
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_UNSPECIFIED
 import androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode
+import com.darkempire78.opencalculator.MyPreferences
 
 //TODO ic_launcher
 //TODO rate app, more app, share app
@@ -15,14 +16,15 @@ import androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode
 //TODO permission ad id
 
 //https://github.com/tplloi/OpenCalc/tree/dev
-class OpenCalcApp : Application() {
+class OpenCalcApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
         // if the theme is overriding the system, the first creation doesn't work properly
         val forceDayNight = MyPreferences(this).forceDayNight
-        if (forceDayNight != MODE_NIGHT_UNSPECIFIED && forceDayNight != MODE_NIGHT_FOLLOW_SYSTEM)
+        if (forceDayNight != MODE_NIGHT_UNSPECIFIED && forceDayNight != MODE_NIGHT_FOLLOW_SYSTEM) {
             setDefaultNightMode(forceDayNight)
+        }
     }
 }
