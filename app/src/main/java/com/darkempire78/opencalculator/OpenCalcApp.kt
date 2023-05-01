6f716f61f41deadc5c9f40639f8405e9ca4e5d6e
loitp <www.muathu@gmail.com>
2023-05-01 16:02:54 +0700

    update

--- app/src/main/java/com/darkempire78/opencalculator/OpenCalcApp.kt
+++ app/src/main/java/com/darkempire78/opencalculator/OpenCalcApp.kt
@@ -1,8 +1,18 @@
 package com.darkempire78.opencalculator
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate.*
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_UNSPECIFIED
+import androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode
 
+//TODO ic_launcher
+//TODO rate app, more app, share app
+//TODO policy
+//TODO firebase
+//TODO ad
+//TODO leak canary
+
+//https://github.com/tplloi/OpenCalc/tree/dev
 class OpenCalcApp : Application() {
 
     override fun onCreate() {
